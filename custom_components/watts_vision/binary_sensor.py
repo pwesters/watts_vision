00674e84969b36bba7f663c4137c3b86f0437ca1
pwesters <35364479+pwesters@users.noreply.github.com>
2022-02-08 11:57:57 +0100

    Update binary_sensor.py

--- custom_components/watts_vision/binary_sensor.py
+++ custom_components/watts_vision/binary_sensor.py
@@ -23,39 +23,45 @@ async def async_setup_platform(
     discovery_info: Optional[DiscoveryInfoType] = None,
 ) -> None:
     """Set up the binary_sensor platform."""
-    wattsClient = hass.data[DOMAIN]['api']
+    wattsClient: WattsApi = hass.data[DOMAIN]['api']
     
     smartHomes = wattsClient.getSmartHomes()
 
     sensors = []
 
-    for y in range(len(smartHomes)):
-        for x in range(len(smartHomes[str(y)]['devices'])):
-            sensors.append(WattsVisionHeatingBinarySensor(wattsClient, smartHomes[str(y)]['smarthome_id'], smartHomes[str(y)]['devices'][str(x)]['id']))
+    if smartHomes is not None:
+        for y in range(len(smartHomes)):
+            if smartHomes[str(y)]['devices'] is not None:
+                for x in range(len(smartHomes[str(y)]['devices'])):
+                    sensors.append(WattsVisionHeatingBinarySensor(wattsClient, smartHomes[str(y)]['smarthome_id'], smartHomes[str(y)]['devices'][str(x)]['id']))
 
     async_add_entities(sensors, update_before_add=True)
 
 class WattsVisionHeatingBinarySensor(BinarySensorEntity):
     """Representation of a Watts Vision thermostat."""
 
-    def __init__(self, wattsClient: WattsApi, smartHome: str, deviceID: str):
+    def __init__(self, wattsClient: WattsApi, smartHome: str, id: str):
         super().__init__()
         self.client = wattsClient
         self.smartHome = smartHome
-        self.deviceID = deviceID
-        self._name = "watts_thermostat_heating"
+        self.id = id
+        self._name = "watts_vision_"
         self._state: bool = False
         self._available = True
     
     @property
     def name(self) -> str:
         """Return the name of the entity."""
-        return self._name + '_' + self.deviceID
+        return self._name + "is_heating"
 
     @property
     def unique_id(self) -> str:
         """Return the unique ID of the sensor."""
-        return 'thermostat_heating_' + self.deviceID
+        return 'thermostat_is_heating_' + self.id
+    
+    @property
+    def device_id(self) -> str:
+        return "watts_vision_watts_thermostat_" + self.id
     
     @property
     def available(self) -> bool:
@@ -69,7 +75,7 @@ class WattsVisionHeatingBinarySensor(BinarySensorEntity):
     
     async def async_update(self):
         try:
-            smartHomeDevice = await self.client.getDevice(self.smartHome, self.deviceID)
+            smartHomeDevice = await self.client.getDevice(self.smartHome, self.id)
             if smartHomeDevice["heating_up"] == '0':
                 self._state = False
             else:
