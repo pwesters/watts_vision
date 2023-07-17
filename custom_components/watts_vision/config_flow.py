30ebf64537e294d29072ec783b97dbf3de5663b9
Coen Warries <coen.warries@student.hu.nl>
2023-07-17 14:55:45 +0200

    feat: OptionsFlowHandler to edit account information

--- custom_components/watts_vision/config_flow.py
+++ custom_components/watts_vision/config_flow.py
@@ -1,9 +1,13 @@
+"""
+Config flow for Watts Vision integration.
+"""
 import logging
 from typing import Any
 
+from homeassistant import config_entries
 from homeassistant.config_entries import CONN_CLASS_CLOUD_POLL, ConfigFlow
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
 from homeassistant.exceptions import HomeAssistantError
 import voluptuous as vol
 
@@ -20,10 +24,17 @@ _LOGGER = logging.getLogger(__name__)
 async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
     """Validate the user input allows us to connect."""
 
+    # Check if the username already exists as an entry
+    existing_entries = hass.config_entries.async_entries(DOMAIN)
+    for entry in existing_entries:
+        if entry.data.get(CONF_USERNAME) == data[CONF_USERNAME]:
+            raise UsernameExists
+
     api = WattsApi(hass, data[CONF_USERNAME], data[CONF_PASSWORD])
 
     authenticated = await hass.async_add_executor_job(api.test_authentication)
 
+    # If authentication fails, raise an exception.
     if not authenticated:
         raise InvalidAuth
 
@@ -48,16 +59,70 @@ class ConfigFlow(ConfigFlow, domain=DOMAIN):
             await validate_input(self.hass, user_input)
         except InvalidAuth:
             errors["base"] = "invalid_auth"
+        except UsernameExists:
+            errors["base"] = "username_exists"
         except Exception:  # pylint: disable=broad-except
             _LOGGER.exception("Unexpected exception")
             errors["base"] = "unknown"
         else:
-            return self.async_create_entry(title=str(user_input['username']), data=user_input)
+            return self.async_create_entry(
+                title=str(user_input["username"]), data=user_input
+            )
 
         return self.async_show_form(
             step_id="user", data_schema=CONFIG_SCHEMA, errors=errors
         )
 
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        """Get the options flow for this handler."""
+        return OptionsFlowHandler(config_entry)
+
 
 class InvalidAuth(HomeAssistantError):
     """Error to indicate there is invalid auth."""
+
+class UsernameExists(HomeAssistantError):
+    """Error to indicate the username already exists."""
+
+class OptionsFlowHandler(config_entries.OptionsFlow):
+    """Handle options flow for the Watts Vision integration."""
+
+    def __init__(self, config_entry):
+        """Initialize the options flow."""
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        errors = {}
+        if user_input is not None:
+            try:
+                _LOGGER.debug("Validate input")
+                validated_data = await validate_input(self.hass, user_input)
+
+                # Update entry
+                _LOGGER.debug("Updating entry")
+                if self.hass.config_entries.async_update_entry(
+                    self.config_entry,
+                    title=str(user_input["username"]),
+                    data=validated_data,
+                ):
+                    # Reload entry
+                    _LOGGER.debug("Reloading entry")
+                    await self.hass.config_entries.async_reload(
+                        self.config_entry.entry_id
+                    )
+
+            except InvalidAuth:
+                errors["base"] = "invalid_auth"
+            except UsernameExists:
+                errors["base"] = "username_exists"
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema({vol.Required(CONF_USERNAME, default=str(self.config_entry.data[CONF_USERNAME])): str,vol.Required(CONF_PASSWORD): str,}),
+            errors=errors,
+        )
