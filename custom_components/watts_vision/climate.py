ed32001340b5683fd7c4769675110388fbf5ef2d
Marcel Mol <marcel@mesa.nl>
2023-07-22 12:19:41 +0200

    climate: only import constants once

--- custom_components/watts_vision/climate.py
+++ custom_components/watts_vision/climate.py
@@ -12,9 +12,6 @@ from homeassistant.components.climate.const import (
     HVAC_MODE_COOL,
     HVAC_MODE_OFF,
     HVAC_MODE_AUTO,
-    PRESET_BOOST,
-    PRESET_COMFORT,
-    PRESET_ECO,
     SUPPORT_PRESET_MODE,
     SUPPORT_TARGET_TEMPERATURE,
 )
